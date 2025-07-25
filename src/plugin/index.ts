7c24b287780465f193e8efa902e679b01e330572
Jesse Day <me@jesseday.dev>
2025-07-25 12:59:07 -0400

    Load stories without calling the function
    Removes the ability to provide a custom name and
    description to the content loader, but fixes a bug that
    did not like calling astro files in the loader.

--- src/plugin/index.ts
+++ src/plugin/index.ts
@@ -6,33 +6,27 @@ import type { AstroIntegration, HookParameters } from "astro";
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 
-type ModuleInterface = {
-  story: {
-    name: string;
-    description: string;
-  };
-};
-
 export function storyLoader() {
   return {
     loader: async () => {
       // globs from the root of the project
       const stories = await import.meta.glob(["/src/**/*.story.astro"]);
-
       const entries = [];
       for (const path in stories) {
-        const entry = stories[path]().then((mod) => {
-          const definition = mod as ModuleInterface;
-          return {
-            id: definition?.story?.name.toLowerCase(),
-            ...definition?.story,
-            path,
-          };
-        });
+        const name = path.split("/").pop()?.replace(/\.story\.astro$/, "");
+        if (!name) {
+          continue;
+        }
+
+        const entry = {
+          id: name?.toLowerCase(),
+          name,
+          path,
+        };
         entries.push(entry);
       }
 
-      return Promise.all(entries);
+      return entries;
     },
   };
 }
